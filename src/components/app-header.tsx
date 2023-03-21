595cb9fb17128337a65c9aa7a6e93051fd591737
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-21 16:36:34 +0100

    Updated the app-header component
    finished the logic for the app-header

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,1 +1,26 @@
-export {};
+import React from 'react'
+import AppBlueLogo from "./../assets/images/logo-blue.svg";
+import AppButton from './app-button';
+
+const AppHeader = ():JSX.Element =>
+{
+
+    const handleButtonClick = ():void =>
+    {
+        //pass
+    }
+    return (
+        <React.Fragment>
+            <section className="container-fluid">
+                <section className="app-logo-container">
+                    <img src={AppBlueLogo} className="img-fluid"/>
+                </section>
+                
+                
+                <section className="connect-wallet-button">
+                    <AppButton className="text-capitalize text-center" text="connect wallet" onClick={handleButtonClick}></AppButton>
+                </section>
+            </section>
+        </React.Fragment>
+    )
+}
