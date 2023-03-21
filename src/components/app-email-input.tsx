166161995500589fa389fdf2e2ec84573eccf1a7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-21 16:49:45 +0100

    Updated the app-email-input

--- src/components/app-email-input.tsx
+++ src/components/app-email-input.tsx
@@ -1,1 +1,14 @@
-export {};
+import React from 'react'
+
+const AppEmailInput = ():JSX.Element =>
+{
+    return(
+        <React.Fragment>
+            <input type="email" name="appEmail" placeholder="Enter your email here" className=""/>
+        </React.Fragment>
+    );
+        
+}
+
+
+export default AppEmailInput;
