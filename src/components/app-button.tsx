2fd1431e0698d2e4a694edf6ece879a0a5ee4caa
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-03-21 16:33:43 +0100

    Updated the button component
    Finished the app-button component logic

--- src/components/app-button.tsx
+++ src/components/app-button.tsx
@@ -1,1 +1,22 @@
-export {};
+import React from 'react'
+
+interface appButtonProps
+{
+    text:string ;
+    className:string;
+    onClick: ()=> void;
+    
+}
+const AppButton = ({text, className, onClick}: appButtonProps):JSX.Element => 
+{
+    return (
+        <React.Fragment>
+            <button className={`app-button ${className}`} onClick={onClick}>{text}</button>
+        </React.Fragment>
+    );
+}
+
+
+
+export default AppButton;
+
