37595ec30e2c1cd9b026e4854de707766af3feba
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-12 12:39:08 +0100

    Bug fix, changed the AJAX settings object

--- src/apis/waitlist.ts
+++ src/apis/waitlist.ts
@@ -1,13 +1,14 @@
 import $ from "jquery";
 
 const connectToBackend = async (email: string): Promise<Response> => {
-    
-  if(!email) return Promise.reject(new Error("pass a valid email address to the function"))
+  if (!email)
+    return Promise.reject(
+      new Error("pass a valid email address to the function")
+    );
   const userEmail = email;
   const apiKEY = process.env.REACT_APP_API_KEY;
   const apiURL = process.env.REACT_APP_API_URL;
-  
-  
+
   return $.ajax({
     url: `${apiURL}`,
     data: {
@@ -19,12 +20,12 @@ const connectToBackend = async (email: string): Promise<Response> => {
       withCredentials: true,
     },
     headers: {
+      "Access-Control-Allow-Origin": "https://heirtrust.vercel.app",
       origin: "https://heirtrust.vercel.app",
       "api-key": apiKEY,
     },
     method: "GET",
   });
-  
 };
 
 export default connectToBackend;
